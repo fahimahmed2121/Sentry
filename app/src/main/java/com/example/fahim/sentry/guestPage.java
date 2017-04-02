0d6b1d11e26391bbbf29dbba605c38f088bf0286
glopz <jonathanglopez@gmail.com>
2017-04-02 08:39:29 -0400

    New

--- app/src/main/java/com/example/fahim/sentry/guestPage.java
+++ app/src/main/java/com/example/fahim/sentry/guestPage.java
@@ -1,7 +1,9 @@
 package com.example.fahim.sentry;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class guestPage extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class guestPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_page);
+    }
+
+    public void toTenantDirectory(View tenantDirectory) {
+        Intent intent = new Intent(this, tenantDirectory);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/fahim/sentry/tenantDirectory.java
@@ -0,0 +1,14 @@
+package com.example.fahim.sentry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tenantDirectory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenant_directory);
+    }
+
+}
