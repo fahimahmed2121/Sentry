16a71c1ed077fa9c5726569cac31000cf881a8e5
Badal <badal.parekh@gmail.com>
2017-04-02 00:02:42 -0400

    Simpler user interface

--- app/src/main/java/com/example/fahim/sentry/HostMain.java
+++ app/src/main/java/com/example/fahim/sentry/HostMain.java
@@ -26,14 +26,10 @@ public class HostMain extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+
+
+
     }
 
     @Override
--- app/src/main/java/com/example/fahim/sentry/RegisterVisitor.java
+++ app/src/main/java/com/example/fahim/sentry/RegisterVisitor.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class RegisterVisitor extends AppCompatActivity {
@@ -16,12 +17,31 @@ public class RegisterVisitor extends AppCompatActivity {
         setContentView(R.layout.activity_register_visitor);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
     public void hostLogin(View hostMain) {
         //TODO show host main page
         Intent intent = new Intent(this, HostMain.class);
         startActivity(intent);
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
 
